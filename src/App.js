51bedaaa8a8e1a2c158a7e7c1fec5c08edc62b00
Meli <MMCalexandra@gmail.com>
2022-05-11 20:50:18 -0600

    Frontend2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Grid, Container, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import fondo from './assets/image/fondo.jpg'
+import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons'
 
-function App() {
+//Objeto de estilos
+const useStyle = makeStyles(theme => ({
+  root: {
+    backgroundImage: `url(${fondo})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh'
+  },
+
+  container: {
+    opacity: '0.8',
+    height: '60%',
+    marginTop: theme.spacing(15),
+    [theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
+      marginTop: 0,
+      width: '100%',
+      height: '100%'
+    }
+  },
+
+  div: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main
+  },
+
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+
+  button: {
+    margin: theme.spacing(2, 0, 2)
+  }
+}))
+//obtiene el password y el nickname
+const App = () => {
+  const [body, setBody] = useState({nickname:'', password:''})
+  const classes = useStyle()
+
+  const handleChange = (e) => {
+       setBody({
+         ...body, // tiene dentro todos los componentes de body
+         [e.target.name]: e.target.value
+       })
+  }
+//boton para mandar archivos
+  const onSubmit =() => {
+    console.log(body)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container component='main' className={classes.root} >
+      <Container component={Paper} elevation={5} maxWidth='xs' className={classes.container} >
+        <div className={classes.div}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'> Inicio de Sesión </Typography>
+          <form className={classes.form}>
+            <TextField
+              fullWidth
+              color='primary'
+              margin='normal'
+              variant='outlined'
+              label='Usuario'
+              name='nickname'
+              value= {body.nickname}
+              onChange= {handleChange}
+              autoComplete= 'off'
+              
+            />
+
+            <TextField
+              fullWidth
+              type='password'
+              color='primary'
+              margin='normal'
+              variant='outlined'
+              label='Contraseña'
+              name='password'
+              value= {body.password}
+              onChange= {handleChange}
+              autoComplete= 'off'
+            />
+            <Button
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.button}
+              onClick= {()=>onSubmit()}
+            >Iniciar Sesión</Button>
+          </form>
+        </div>
+      </Container>
+    </Grid>
   );
 }
 
-export default App;
+export default App
